d5e85f09da7293fc2624d5009dfbe977a4089ff5
hhhwlgns <hhhwlgns@gmail.com>
2025-02-24 13:15:34 +0900

    day01 pandas v0.2

--- day01.py
+++ day01.py
@@ -1,9 +1,19 @@
 import numpy as np
+import pandas as pd
 
-v = np.array([1, 3, -9, 2])
-print(v, v.shape)
-b = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
-print(b, b.shape)
-c = np.array([[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]])
-print(c, c.shape)
-print(c.strides)
+df = pd.DataFrame(
+    {"a":[4,5,6],
+     "b":[7,8,9],
+     "c":[10,11,12]}, index = [1, 2, 3]
+)
+
+print(df)
+
+nf = pd.DataFrame(
+    [[4, 7, 10],
+     [5, 8, 11],
+     [6, 9, 12]],
+     index = [1, 2, 3], columns = ['a', 'b', 'c']
+)
+
+print(nf)
